d39d40ad8855802f83fe9c02e775a5822bf73e29
phalanity <phalanity2@gmail.com>
2022-11-10 13:20:40 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+//import './App.css';
+import { VRButton, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import { Canvas } from '@react-three/fiber'
+
+function App() {
+  return (
+    <>
+      <ARButton />
+      <Canvas>
+        <XR>
+          <Controllers />
+          <Hands />
+          <mesh>
+            <boxGeometry />
+            <meshBasicMaterial color="blue" />
+          </mesh>
+        </XR>
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
